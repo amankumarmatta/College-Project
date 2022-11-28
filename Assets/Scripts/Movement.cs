2d1cc0b5b290be85d22a28057f371c8cfa4b7648
Aman Kumar Matta <amankumarmatta0@gmail.com>
2022-11-28 14:57:15 +0530

    Minor Fixes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,10 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator anim;
+
+    PlayerControls controls;
+    private float direction = 0f;
+    public float speed;
+    public float JumpForce;
+
+    private bool isGrounded = false;
+    private bool isFacingRight = true;
+
+    public Rigidbody2D rB;
+
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Enable();
+
+        controls.Move.Move.performed += ctx =>
+        {
+            direction = ctx.ReadValue<float>();
+        };
+
+        controls.Move.Jump.performed += ctx => Jump();
+    }
+
+    private void Update()
+    {
+        rB.velocity = new Vector2(direction * speed, rB.velocity.y);
+
+        if (isFacingRight && direction < 0 || !isFacingRight && direction > 0)
+        {
+            Flip();
+        }
+
+        AnimParams();
+    }
+
+    private void AnimParams()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(direction));
+        anim.SetBool("Grounded", isGrounded);
+    }
+
+    public void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded == true)
+        {
+            rB.velocity = new Vector2(rB.velocity.x, JumpForce);
+        }
+        isGrounded = false;
+        anim.SetTrigger("Jump");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
 }
